4e8f3e91c7d98a1a39be7cce5410bdb8a857b971
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2023-04-24 15:01:37 +0300

    t001

---
+++ Task001S2.java
@@ -0,0 +1,34 @@
+// 1) Дана последовательность N целых чисел. Найти сумму простых чисел.
+public class Task001S2 {
+    public static void main(String[] args) {
+        String number="2,6,7,11,1";
+        System.out.println(sumPrimeNumber(number));
+    }
+
+    private static int sumPrimeNumber(String number) {
+        String [] arrNumberString= number.split(",");
+        int arrNumber;
+        int sum=0;
+        for (int i = 0; i < arrNumberString.length; i++) {
+            arrNumber=Integer.parseInt(arrNumberString[i]);
+            if(primeNumber(arrNumber)){
+                sum+=arrNumber;
+            }
+        }
+        return sum;
+    }
+
+    private static boolean primeNumber(int s) {
+        boolean j=false;
+        if (s==1){
+            j=true;
+        }
+        for (int i = 2; i < s; i++) {
+            j=(s%i!=0);
+            if (!j){
+            break;
+            }
+        }
+        return j;
+    }
+}
